9efb49d7a69e46b811252a703eb2f71f0549bb77
Gayatri <khairkargayatri91@gmail.com>
2025-09-08 15:52:29 +0530

    remove emai key

--- web_portal/settings.py
+++ web_portal/settings.py
@@ -139,8 +139,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'gayakishorkhairkar@gmail.com'
-EMAIL_HOST_PASSWORD = 'gaqs gvwv yxwx vsmc'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 SIMPLE_JWT = {
